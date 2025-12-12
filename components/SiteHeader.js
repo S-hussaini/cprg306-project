9d2c709010d257dcc5ceb6018ad2c95d58ed8145
S-hussaini <Sheeba.Hussaini@edu.sait.ca>
2025-12-12 13:46:37 -0700

    i add the logout button

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -1,6 +1,17 @@
+"use client";
 import Link from "next/link";
+import { useUserAuth } from "../lib/auth-context";
 
 export default function PageHeader() {
+  const{ user, firebaseSignOut } = useUserAuth();
+
+  const handleLogout = async () => {
+    try {
+      await firebaseSignOut();
+    } catch (err) {
+      console.error("Logout failed:", err);
+    }
+  };
   return (
     <header className="absolute top-0 left-0 w-full z-50">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-end items-center text-white">
@@ -8,7 +19,21 @@ export default function PageHeader() {
           <Link href="/" className="hover:text-yellow-400">Home</Link>
           <Link href="/tasks" className="hover:text-yellow-400">Tasks</Link>
           <Link href="/contact" className="hover:text-yellow-400">Contact Us</Link>
-          <Link href="/login" className="hover:text-yellow-400">Login</Link>
+
+          {!user && (
+            <Link href="/login" className="hover:text-yellow-400">Login</Link>
+          )}
+          {user && (
+            <>
+              <Link href="/profile" className="hover:text-yellow-400">Profile</Link>
+              <button
+                onClick={handleLogout}
+                className="hover:text-yellow-400"
+              >
+                Logout
+              </button>
+            </>
+          )}
         </nav>
       </div>
     </header>
